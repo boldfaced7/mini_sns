196671e65fc1863451209a1b18c70d4714d77e7f
준모 강 <boldfaced7@hanyang.ac.kr>
2022-09-01 19:55:15 +0900

    feat: templateHandler

---
+++ src/api_gateway_service/service/template.go
@@ -0,0 +1,29 @@
+package service
+
+import (
+	"github.com/stretchr/objx"
+	"html/template"
+	"net/http"
+	"path/filepath"
+	"sync"
+)
+
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+
+	data := map[string]interface{}{
+		"Host": r.Host,
+	}
+	if authCookie, err := r.Cookie("auth"); err == nil {
+		data["UserData"] = objx.MustFromBase64(authCookie.Value)
+	}
+	t.templ.Execute(w, data)
+}
