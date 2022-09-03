0f9913a9eaf4971f3b8e70ef742c51a8371a7659
준모 강 <boldfaced7@hanyang.ac.kr>
2022-09-03 11:53:59 +0900

    feat: 라우팅 추가 구현

--- src/api_gateway_service/service/api_gateway_service.go
+++ src/api_gateway_service/service/api_gateway_service.go
@@ -13,14 +13,16 @@ func Run() {
 	flag.Parse()
 
 	gomniauth.SetSecurityKey("SECURITY KEY")
-	gomniauth.WithProviders(google.New(
-		"key",
-		"Secret",
-		"http://localhost:8080/auth/callback/google",
-	))
+	gomniauth.WithProviders(
+		google.New(
+			"624312092698-n5djffqsu9cocq008fr729osg7657l8m.apps.googleusercontent.com",
+			"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",
+			"http://localhost:8080/auth/callback/google",
+		))
 
 	http.Handle("/login", &templateHandler{filename: "login.html"})
-	http.Handle("/auth/", loginHandler)
+	http.HandleFunc("/auth/login/", loginHandler)
+	http.HandleFunc("/auth/callback/", callbackHandler)
 
 	log.Println("Starting api gateway service", *addr)
 	if err := http.ListenAndServe(*addr, nil); err != nil {
